cb08b7abcbea000df1e78823c0eedcbccb7a5b54
RainaBrighu <brighuraina9999@gmail.com>
2022-02-22 17:02:54 +0530

    This is assignment

---
+++ src/main/java/com/tekion/cricketmatch/api/TeamController.java
@@ -0,0 +1,32 @@
+package com.tekion.cricketmatch.api;
+
+import com.tekion.cricketmatch.services.TeamService;
+import com.tekion.cricketmatch.services.beans.team.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TeamController {
+
+    @Autowired
+    TeamService teamService;
+
+    @GetMapping("/team")
+    public List<Team> getAllTeams() {
+        return teamService.getAllTeams();
+    }
+
+    @GetMapping("/team/{teamId}")
+    public Team getTeam(@PathVariable int teamId) {
+        return teamService.getTeam(teamId);
+    }
+    @PostMapping("/add/{teamId}/{teamName}")
+    public Team addTeam(@PathVariable int teamId, @PathVariable String teamName) {
+        return teamService.addTeam(teamId,teamName);
+    }
+}
